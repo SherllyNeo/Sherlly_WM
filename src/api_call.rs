a83d52ef23d02b535c52f839f6acf3a4d2e213cf
sherllyneo <sherlly.neomail@gmail.com>
2023-01-15 12:30:36 +0000

    remove emoji

--- src/api_call.rs
+++ src/api_call.rs
@@ -17,7 +17,24 @@ pub fn api_call(url: &str) -> Option<String> {
               }
     let second = time::Duration::from_secs(1);
     let re = Regex::new("\n").unwrap();
-    let response_text = re.replace_all(&response_text," ");
+    let mut response_text = re.replace_all(&response_text," ");
+    let regex = Regex::new(concat!(
+                     "[",
+                             "\u{01F600}-\u{01F64F}", // emoticons
+                                "\u{01F300}-\u{01F5FF}", 
+                                                      //
+                                                      //        symbols & pictographs
+                             "\u{01F680}-\u{01F6FF}",
+                                       //                 // transport & map symbols
+                             "\u{01F1E0}-\u{01F1FF}",
+                                                      //                         // flags (iOS)
+                             "\u{002702}-\u{0027B0}",
+                             "\u{0024C2}-\u{01F251}",
+                              "]+",
+                                 ))
+                              .unwrap();
+  
+    let response_text = regex.replace_all(&response_text,"").to_string();
     thread::sleep(second);
     
     
