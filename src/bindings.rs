5cb96f621d4ae0fa8c083795ffb05a2290b3e62a
sherllyneo <sherlly.neomail@gmail.com>
2023-01-30 17:34:52 +0000

    changed from maim to flameshot for screenshots

--- src/bindings.rs
+++ src/bindings.rs
@@ -52,7 +52,7 @@ where
         "M-Left" => send_layout_message(|| ShrinkMain),
 
         // Launchers
-        "M-A-s" => spawn("maim -s new_screenshot.png"),
+        "M-A-s" => spawn("flameshot gui"), //screenshot
         "M-d" => spawn("dmenu_run"),
         "M-t" => spawn("st"),
         "M-S-t" => spawn("to_do"), //to do program
